ec583a0a5550d32b776799f8a8060f9a0b65e86c
Mia9469 <471559400@qq.com>
2020-09-27 14:42:34 +0800

    Create read_dicom_job.m

---
+++ read_dicom_job.m
@@ -0,0 +1,11 @@
+%-----------------------------------------------------------------------
+% Job saved on 16-Apr-2019 03:24:58 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6906)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.util.import.dicom.data = '<UNDEFINED>';
+matlabbatch{1}.spm.util.import.dicom.root = 'flat';
+matlabbatch{1}.spm.util.import.dicom.outdir = '<UNDEFINED>';
+matlabbatch{1}.spm.util.import.dicom.protfilter = '.*';
+matlabbatch{1}.spm.util.import.dicom.convopts.format = 'nii';
+matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;
